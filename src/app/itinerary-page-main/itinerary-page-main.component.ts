0d15658e9d8707574e5ec8c638b7a101c925a0ef
anilmelinmani <anilmelinmani02@gmail.com>
2023-12-27 10:06:50 +0530

    integrated ol map

--- src/app/itinerary-page-main/itinerary-page-main.component.ts
+++ src/app/itinerary-page-main/itinerary-page-main.component.ts
@@ -1,5 +1,13 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, Renderer2 } from '@angular/core';
 import { ItineraryService } from '../services/itinerary.service';
+import Map from 'ol/Map';
+import TileLayer from 'ol/layer/Tile';
+import OSM from 'ol/source/OSM';
+import View from 'ol/View';
+import Overlay from 'ol/Overlay';
+import { fromLonLat } from 'ol/proj';
+import { boundingExtent } from 'ol/extent';
+import { Coordinate } from 'ol/coordinate';
 
 @Component({
   selector: 'app-itinerary-page-main',
@@ -16,11 +24,22 @@ localCuisine: any[] = [];
 highRatetRestaurants: any[] = [];
 cityDetails: any = {};
 suggestionsData: any[] = [];
-triviaData: any[] = []
+triviaData: any[] = [];
+map: Map = new Map();
+markerOverlay !: Overlay;
+allCoords: any[] = []
 
-constructor(private itineraryService: ItineraryService) {
+
+constructor(
+  private itineraryService: ItineraryService, 
+  private renderer: Renderer2
+  ) {
   const selectedCity = 'Pune';
   this.fetchItineraryData(selectedCity);
+}
+
+ngAfterViewInit() {
+  this.initializeMap();
 }
 
 fetchItineraryData(selectedCity: string): void {
@@ -39,14 +58,64 @@ fetchItineraryData(selectedCity: string): void {
     this.triviaData = this.itineraryData.trivia;
     
       this.cardsData = data.data.tripPlans[0].activities ;   
-      // console.log('allPlanes',this.cardsData);
+      console.log('allPlanes',this.cardsData);
+      // extract coordinates
+      this.cardsData.forEach((e)=>{
+        this.allCoords.push(e.coordinates)
+      })
 
       this.fetchCommentData();
+      this.addMarker();
     },
     (error) => {
       console.error('Error fetching itinerary data:', error);
     }
   );
+}
+
+initializeMap() {
+  console.log('initializing map');
+  
+  this.map = new Map({
+    target: 'map',
+    layers: [
+      new TileLayer({
+        source: new OSM()
+      })
+    ],
+    view: new View({
+      zoom: 6
+    })
+  });
+}
+
+addMarker() {
+  const markerOverlays = this.allCoords.map(coord => {
+    const markerElement = this.renderer.createElement('div');
+    markerElement.innerHTML = '<img src="../../assets/img/location-pin.png" alt="Marker" style="width: 32px; height: 32px;">';
+
+    const markerOverlay = new Overlay({
+      position: fromLonLat([parseFloat(coord.longitude), parseFloat(coord.latitude)]),
+      element: markerElement,
+      positioning: 'center-center',
+      offset: [0, -16]
+    });
+
+    this.map.addOverlay(markerOverlay);
+    return markerOverlay;
+  });
+  // Get all marker coordinates and filter out undefined values
+  const markerCoordinates = markerOverlays
+    .map(overlay => overlay.getPosition())
+    .filter(coord => coord !== undefined) as Coordinate[];
+
+  if (markerCoordinates.length > 0) {
+    // Calculate bounding box
+    const boundingBox = boundingExtent(markerCoordinates);
+
+    // Fit the view to the bounding box
+    this.map.getView().fit(boundingBox, { padding: [20, 20, 20, 20], maxZoom: 13 });
+  }
 }
 
 fetchCommentData(){
